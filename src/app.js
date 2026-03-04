4231ffad7efb15a6a41a77f89f2bc6f4b7984116
Franco Andres Diaz <dfrancoandres@gmail.com>
2026-03-04 20:53:34 -0300

    add cors origin

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ import router from "./routes/index.js"
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: [FRONT_URL]
+}));
 app.use(express.json());
 app.use('/api', router)
 
